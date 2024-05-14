87e0a4975a0b1717e08270c85bb28eefa1845d83
Stephen Clemmer <stephenjclemmer@gmail.com>
2024-05-14 13:56:59 -0700

    added lintr, gitignore, and package.json

--- server.js
+++ server.js
@@ -2,3 +2,28 @@
 
 const express = require("express");
 const app = express();
+const PORT = 3000;
+
+const router = express.Router();
+
+// CREATE
+app.post("/", (req, res, next) => {
+  res.send("hello world");
+});
+
+// READ
+app.get("/", (req, res, next) => {
+  res.send("hello world");
+});
+
+// UPDATE
+app.put("/", (req, res, next) => {
+  res.send("hello world");
+});
+
+// DELETE
+app.delete("/", (req, res, next) => {
+  res.send("hello world");
+});
+
+app.listen(3000);
